ffa9387beabbe62d107d79858da8f75676cf6e7a
Noah Hellen <noahhellen@gmail.com>
2025-06-07 17:20:45 +0100

    LCD screen working on Arduino

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # birdy <img src="figures/birdy.png" align="right" height="138" /></a>
 
-Ferros is a unrelenting planet; the strong magnetic field  
+Ferros is a unrelenting planet; the formidable magnetic field has made inhabiting this planet almost impossible.
+
+Humanity is faced with many challenges to sustain life on Ferros:
+
+* Food security
+* Communication
+* Transport
+
+The advent of **birdy** will make communication problems a thing of the past!
+
+
+
 
 birdy is the communication of the future on ferros!
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,22 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-const int sensorPin = A0;
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int sensor_pin = A0;
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-void setup() {
+
+void setup(){
   Serial.begin(9600);
-  pinMode(sensorPin, INPUT);
-  lcd.begin(16, 2);
-  lcd.print("Hello!");
-  delay(2000); // Give time to read
-  lcd.clear(); // Clear after welcome message
+  pinMode(sensor_pin, INPUT);
+  lcd.print("Hello Ferros!");
 }
 
-void loop() {
-  int sensorVal = analogRead(sensorPin);
-  float voltage = (sensorVal / 1023.0) * 5.0;
-
+void loop(){
+  int sensor_val = analogRead(sensor_pin);
+  float voltage = (sensor_val / 1023.0) * 5.0;
   Serial.print("Sensor Value: ");
-  Serial.print(sensorVal);
+  Serial.print(sensor_val);
   Serial.print(", Volts: ");
   Serial.println(voltage);
-
-  lcd.setCursor(0, 0);
-  lcd.print("Sensor: ");
-  lcd.print(sensorVal);
-
-  lcd.setCursor(0, 1);
-  lcd.print("Volt: ");
-  lcd.print(voltage, 2);  // Print voltage with 2 decimal places
-
-  delay(1000);  // Delay before next reading
+  delay(500);
 }
